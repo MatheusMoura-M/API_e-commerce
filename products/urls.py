2529939c34ed455e00e4459318280cfffccb1785
Matheus M <matheus150101miranda@gmail.com>
2023-03-08 15:51:11 -0300

    add views

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,8 @@
 from . import views
 from django.urls import path
-from .views import ProductView, ProductDetailView
+from .views import ProductView
 
 
 urlpatterns = [
     path("products/", views.ProductView.as_view()),
-    path("products/<product_id>/", views.ProductDetailView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Product
 from .serializers import ProductSerializer
-import ipdb
 
 
 class ProductView(generics.ListCreateAPIView):
@@ -12,20 +11,21 @@ class ProductView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    def perform_create(self, serializer):
-        return super().perform_create(serializer)
-
-
-class ProductDetailView(generics.ListCreateAPIView):
-    authentication_classes = [JWTAuthentication]
-    # permission_classes = [...]
-
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
     def get_queryset(self):
-        # ipdb.set_trace()
-        return Product.objects.filter(pk=self.kwargs["product_id"])
+        id_parameter = self.request.query_params.get("id")
+        name_parameter = self.request.query_params.get("name")
+        category_parameter = self.request.query_params.get("category")
+
+        if id_parameter:
+            return Product.objects.filter(pk=id_parameter)
+
+        if name_parameter:
+            return Product.objects.filter(name__icontains=name_parameter)
+
+        if category_parameter:
+            return Product.objects.filter(category__icontains=category_parameter)
+
+        return super().get_queryset()
 
     def perform_create(self, serializer):
         return super().perform_create(serializer)
