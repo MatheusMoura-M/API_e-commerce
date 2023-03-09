7a3e4e98f115b19ec5b19c3d5a83c0b525e37740
Matheus M <matheus150101miranda@gmail.com>
2023-03-09 10:23:57 -0300

    add method Patch and update of the Post

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
-from .views import ProductView
+from .views import ProductView, ProductDetailView
 
 
 urlpatterns = [
     path("products/", views.ProductView.as_view()),
+    path("products/<uuid:product_id>", views.ProductDetailView.as_view()),
 ]
