a5c7039849eeb5b5f905c788749bfe999c6dfc99
Matheus M <matheus150101miranda@gmail.com>
2023-03-10 17:03:57 -0300

    att

--- __tests__/mocks/users/admin.mock.py
+++
@@ -1,7 +0,0 @@
-admin_data = {
-    "first_name": "Mariano",
-    "username": "marianodias",
-    "email": "marianodias@gmail.com",
-    "password": "123123",
-    "is_superuser": True,
-}
--- __tests__/mocks/users/seller.mock.py
+++
@@ -1,7 +0,0 @@
-seller_data = {
-    "first_name": "Josival",
-    "username": "josival123",
-    "email": "josival123@gmail.com",
-    "password": "123123",
-    "is_seller": True,
-}
