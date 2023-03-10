b42a8070c661e6d13fa5fee726b4b6f1f75b2870
Bruno <bruno3p33@gmail.com>
2023-03-09 21:47:21 -0300

    feat:Order atualizar e enviar email

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,6 +1,9 @@
+from users.models import User
 from utils.fields.order_fields import OrderFields
 from .models import Order
 from rest_framework import serializers
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -9,10 +12,18 @@ class OrderSerializer(serializers.ModelSerializer):
         
         for key, value in validated_data.items():
             setattr(instance, key, value)
+        
+            send_mail(
+                subject='Pedido Alterado',
+                message=['Pedido Alterado com sucesso para', Order.status],
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[User.email],
+                fail_silently=False
+            )
+            instance.save()
+        # shell do Django
 
-        instance.save()
-
-        return instance
+            return instance
      
     class Meta:
         model = Order
